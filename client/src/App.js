5aa6dc92b617774fee3e8c8078bfdc0c4b6899be
unknown <vs6838635@gmail.com>
2024-08-14 00:33:40 +0530

    Posts schema, routing, get and post posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/blog1.png';
+import useStyles from './styles.js';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>Hellllo!</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60"></img>
+            </AppBar>
+            <grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={7}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </grow>
+        </Container>
     )
 }
 
