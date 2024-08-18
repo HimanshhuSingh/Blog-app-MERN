8f1dcc8cb98fa0bdeea346754b836979a8f05ee1
unknown <vs6838635@gmail.com>
2024-08-18 22:16:31 +0530

    Fixed submit button

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -21,11 +21,11 @@ const Form = () => {
     }
   return (
     <Paper className={classes.paper}>
-      <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onsubmit={handleSubmit}>
+      <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6">Creating a memory</Typography>
         <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e)=>setPostData({ ...postData, creator: e.target.value})} />
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e)=>setPostData({ ...postData, title: e.target.value})} />
-        <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e)=>setPostData({ ...postData, message: e.target.value})} />
+        <TextField name="message" variant="outlined" label="Message" fullWidth multiline value={postData.message} onChange={(e)=>setPostData({ ...postData, message: e.target.value})} />
         <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e)=>setPostData({ ...postData, tags: e.target.value})} />
         <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64})} /></div>
         <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
