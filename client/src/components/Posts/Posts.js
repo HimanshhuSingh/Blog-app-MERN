56d412e02d4214f8fe47ff09ee0d60ca2a02f1ea
unknown <vs6838635@gmail.com>
2024-08-19 23:23:55 +0530

    added post component and fixed tags

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core'
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
@@ -8,11 +9,15 @@ const Posts = () => {
   const posts = useSelector((state)=>state.posts);
     const classes = useStyles();
   return (
-    <>
-    <h1 className={classes.something}></h1>
-    <Post />
-    <Post />
-    </>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   )
 }
 
